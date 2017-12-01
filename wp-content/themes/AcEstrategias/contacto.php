c5301ad3b72fbd17e3ccc16599a4fcb291ea422c
desbog7 <desbog7@DESBOG7>
2017-12-01 15:04:21 -0500

    sql 2

---
+++ wp-content/themes/AcEstrategias/contacto.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: desbog7
+ * Date: 01/12/2017
+ * Time: 2:29 PM
+ */
--- wp-content/themes/AcEstrategias/single.php
+++ wp-content/themes/AcEstrategias/single.php
@@ -1,3 +1,21 @@
-<?php get_template_part('templates/content-single', get_post_type()); ?>
+<?php
+/**
+ * Template Name: contacto-tp
+ */
+?>
 
 
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+        </div>
+        <div class="col-12 col-sm-6">
+            <?php if (qtrans_getLanguage() == "es") : ?>
+                <?php echo do_shortcode('[contact-form-7 id="178" title="Cantacto Ep"]') ?>
+            <?php endif ?>
+            <?php if (qtrans_getLanguage() == "en") : ?>
+                <?php echo do_shortcode('[contact-form-7 id="178" title="Cantacto Ep"]') ?>
+            <?php endif ?>
+        </div>
+    </div>
+</div>
