6d5c0903c7b62e2adf3ad37cd23261872d57c91a
desbog7 <desbog7@DESBOG7>
2017-12-05 16:58:21 -0500

    ajuste navbar

--- wp-content/themes/AcEstrategias/templates/header.php
+++ wp-content/themes/AcEstrategias/templates/header.php
@@ -1,28 +1,29 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" role="navigation">
-                <?php
-                if (has_nav_menu('primary_navigation')) :
-                    wp_nav_menu([
-                        'theme_location' => 'primary_navigation',
-                        'menu' => 'top_menu',
-                        'depth' => 2,
-                        //'container' => 'div',
-                        'menu_class' => 'navbar-nav',
-                        'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                        'walker' => new wp_bootstrap_navwalker()
-                    ]);
-                endif;
-                ?>
-            </div>
-        </nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu([
+                'theme_location' => 'primary_navigation',
+                'menu' => 'top_menu',
+                'depth' => 2,
+                //'container' => 'div',
+                'menu_class' => 'navbar-nav mr-auto',
+                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                'walker' => new wp_bootstrap_navwalker()
+            ]);
+        endif;
+        ?>
     </div>
+</nav>
+
+
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
 </header>
 
 
