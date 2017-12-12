628947dab973498d0a40cb64ba7a9395b63222b4
desbog7 <desbog7@DESBOG7>
2017-12-12 10:59:07 -0500

    ajustes 12/12/07

--- wp-content/themes/AcEstrategias/contacto.php
+++ wp-content/themes/AcEstrategias/contacto.php
@@ -4,34 +4,96 @@
  */
 ?>
 
-<div class="layout__servicios">
+<div class="layout__contacto">
     <div class="container">
-        <div class="row">
-            <div class="col-12 col-sm-6">
-                <div class="contactInformation">
-                    <div class="contactInformation__address">
-                        <div class="title"></div>
-                        <div class="info"></div>
-                    </div>
-                    <div class="contactInformation__email">
-                        div class="title">
-                    </div>
-                    <div class="info"></div>
-                </div>
-                <div class="contactInformation__tel">
-                    div class="title">
-                </div>
-                <div class="info"></div>
-            </div>
 
-            <div class="col-12 col-sm-6">
-                <?php if (qtrans_getLanguage() == "es") : ?>
-                    <?php echo do_shortcode('[contact-form-7 id="178" title="Cantacto Ep"]') ?>
-                <?php endif ?>
-                <?php if (qtrans_getLanguage() == "en") : ?>
-                    <?php echo do_shortcode('[contact-form-7 id="178" title="Cantacto Ep"]') ?>
-                <?php endif ?>
+        <div class="row">
+            <div class="col-12">
+                <div class="title__layout__contacto">
+                    <h1>Contacto</h1>
+                </div>
             </div>
         </div>
+
+        <div class="warp__contacto">
+            <div class="row">
+                <div class="col-12 col-sm-6">
+                    <div class="contactInformation">
+                        <?php if (get_field('direccion')): ?>
+                            <div class="contactInformation__address">
+                                <?php if (get_field('direccion')['titulo_direccion']): ?>
+                                    <div class="title">
+                                        <h2>
+                                            <?php echo get_field('direccion')['titulo_direccion']; ?>
+                                        </h2>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if (get_field('direccion')['info_direccion']): ?>
+                                    <div class="info">
+                                        <p>
+                                            <?php echo get_field('direccion')['info_direccion']; ?>
+                                        </p>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if (get_field('email')): ?>
+                            <div class="contactInformation__email">
+                                <?php if (get_field('email')['titulo_email']): ?>
+                                    <div class="title">
+                                        <h2>
+                                            <?php echo get_field('email')['titulo_email']; ?>
+                                        </h2>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if (get_field('email')['info_email']): ?>
+                                    <div class="info">
+                                        <p>
+                                            <?php echo get_field('email')['info_email']; ?>
+                                        </p>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if (get_field('telefono')): ?>
+                            <div class="contactInformation__telefono">
+                                <?php if (get_field('telefono')['titulo_telefono']): ?>
+                                    <div class="title">
+                                        <h2>
+                                            <?php echo get_field('telefono')['titulo_telefono']; ?>
+                                        </h2>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if (get_field('telefono')['info_telefono']): ?>
+                                    <div class="info">
+                                        <p>
+                                            <?php echo get_field('telefono')['info_telefono']; ?>
+                                        </p>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+
+
+                </div>
+                <div class="col-12 col-sm-6">
+                    <div class="form__contancto">
+                        <?php if (qtrans_getLanguage() == "es") : ?>
+                            <?php echo do_shortcode('[contact-form-7 id="178" title="Cantacto Es"]') ?>
+                        <?php endif ?>
+                        <?php if (qtrans_getLanguage() == "en") : ?>
+                            <?php echo do_shortcode('[contact-form-7 id="207" title="Cantacto En"]') ?>
+                        <?php endif ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
+
+
+
